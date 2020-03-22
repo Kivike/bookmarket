f0c5ba78859d81227b4798449751fadc57943593
Kivike <coloris23@gmail.com>
2020-03-22 20:15:57 +0200

    Marketplace activity

--- app/src/main/java/fi/oulu/bookmarket2020/MarketplaceActivity.kt
+++ app/src/main/java/fi/oulu/bookmarket2020/MarketplaceActivity.kt
@@ -2,11 +2,61 @@ package fi.oulu.bookmarket2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.content_collection.view.*
 
-class MarketplaceActivity : AppCompatActivity() {
+class MarketplaceActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_marketplace)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        toolbar = findViewById(R.id.toolbar)
+        navView = findViewById(R.id.nav_view)
+
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        refreshCollectionList()
+    }
+
+    private fun refreshCollectionList() {
+        val reminders: MutableList<String> = ArrayList()
+
+        for (i in 1..15) {
+            reminders.add("Book name${i}")
+        }
+
+        val adapter = MarketplaceAdapter(applicationContext, reminders)
+        content.collection_list.adapter = adapter
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this, "Clickity click", Toast.LENGTH_SHORT).show()
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
---
+++ app/src/main/java/fi/oulu/bookmarket2020/MarketplaceAdapter.kt
@@ -0,0 +1,41 @@
+package fi.oulu.bookmarket2020
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.collection_list_item.view.*
+
+class MarketplaceAdapter(context: Context, private val list: List<String>) : BaseAdapter() {
+
+    private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val row = inflater.inflate(R.layout.collection_list_item, parent, false)
+
+        row.book_name.text = list[position]
+        row.book_author.text ="Firstname Lastname"
+        row.sale_status.text = ""
+        row.imageButton.visibility = View.GONE
+
+        if (position % 4 != 0) {
+            row.sale_status.text = position.toString() + " €"
+            row.sale_status.textSize = 18.0f
+        }
+        return row
+    }
+
+    override fun getItem(position: Int): Any {
+        return list[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return list.size
+    }
+
+}
