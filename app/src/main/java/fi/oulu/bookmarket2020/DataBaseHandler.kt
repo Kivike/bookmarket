03abb43b40131ad464e85dca8a23ae74c7ca9d3d
Manish Maharjan <developermanishid@gmail.com>
2020-04-13 17:40:12 +0300

    added list of required column

--- app/src/main/java/fi/oulu/bookmarket2020/DataBaseHandler.kt
+++ app/src/main/java/fi/oulu/bookmarket2020/DataBaseHandler.kt
@@ -16,13 +16,33 @@ val COL_BOOK_NAME = "book_name"
 val COL_BOOK_SALEABLE ="book_saleable"
 val COL_BOOK_READ = "book_already_read"
 val COL_BOOK_CONDITION = "book_condition"
+val COL_ISBN_NUMBER = "isbn number"
+val COL_PRICE = "price"
+val COL_COMMENTS = "comments"
+val COL_EDITION = "edition"
+val COL_AUTHOR = "author"
+val COL_FIRST_NAME = "first name"
+val COL_LAST_NAME = "last name"
+val COL_EMAIL_ADDRESS = "email address"
+val COL_PHONE = "phone"
+val COL_PASSWORD = "password"
 
 
 class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1){
     override fun onCreate(db: SQLiteDatabase?) {
         val createTable = "CREATE TABLE " + TABLE_NAME + " (" +
                 COL_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL_FIRST_NAME + "VARCHAR(256)"+
+                COL_LAST_NAME + "VARCHAR(256)"+
+                COL_EMAIL_ADDRESS + "VARCHAR(256)"+
+                COL_PHONE + "INTEGER" +
+                COL_PASSWORD + "VARCHAR(256)"+
                 COL_BOOK_NAME + "VARCHAR(256)," +
+                COL_AUTHOR + "VARCHAR(256)," +
+                COL_ISBN_NUMBER + "INTEGER" +
+                COL_EDITION + "INTEGER" +
+                COL_PRICE + "INTEGER" +
+                COL_COMMENTS + "VARCHAR(256)," +
                 COL_BOOK_CONDITION + "VARCHAR(256)," +
                 COL_BOOK_READ + "INTEGER DEFAULT 0," +
                 COL_PERSON_NAME + "VARCHAR(256)," +
@@ -32,12 +52,13 @@ class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context, DATABASE
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db?.execSQL("DROP TABLE IF EXITS" + TABLE_NAME)
+        onCreate(db)
     }
 
     fun inserData(bookmarket : MarketplaceAdapter) {
         //Getting the Write Database
         //TODO: get the content values for needed content
-        val id = UUID.randomUUID().toString()
         val db = this.writableDatabase
         var cv = ContentValues()
         cv.put(COL_BOOK_NAME, bookmarket.book_name)
