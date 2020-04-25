5c92ce3c26994c7799ad13f56f83ff8d95aecf36
Manish Maharjan <developermanishid@gmail.com>
2020-04-25 19:45:38 +0300

    added functionality for purchasing with buy button and showing success page

--- app/src/main/java/fi/oulu/bookmarket2020/BuyBookActivity.kt
+++ app/src/main/java/fi/oulu/bookmarket2020/BuyBookActivity.kt
@@ -1,7 +1,9 @@
 package fi.oulu.bookmarket2020
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class BuyBookActivity : AppCompatActivity() {
@@ -11,6 +13,7 @@ class BuyBookActivity : AppCompatActivity() {
         setContentView(R.layout.activity_buy_book)
 
         configureToolbar()
+        onBuybuttonClick89()
     }
 
     private fun configureToolbar() {
@@ -19,5 +22,33 @@ class BuyBookActivity : AppCompatActivity() {
         supportActionBar?.title = "Buy book"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    // When buy button is clicked
+    /*
+     * purchased books need to be visible in db
+     * buy button should have on click listener and that should take to order placed activity
+     *
+     *
+     *
+        row.collection_list_items.setOnClickListener {
+            startBuyBookActivity(book)
+        }
+     */
+
+    private fun onBuybuttonClick89() {
+        val abb_buy = findViewById<Button>(R.id.abb_buy)
+        abb_buy.setOnClickListener {
+            //1. goto order placed
+            /**
+             *         val intent = Intent(applicationContext, BuyBookActivity::class.java)
+            intent.putExtra("book_id", book.uid)
+            activityContext.startActivity(intent)
+             */
+            val intent = Intent(applicationContext, OrderSuccessActivity::class.java )
+            startActivity(intent)
+
+            //2. store in db
+        }
     }
 }
